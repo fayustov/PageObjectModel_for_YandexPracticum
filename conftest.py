cea33efcdfd56c965cd09a06eb3267c462e4fb2f
fayustov <fayustov26@mail.ru>
2024-03-31 23:54:57 +0300

    Sprint 6 project added

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    service = Service(GeckoDriverManager().install())
+    options = Options()
+    options.add_argument("--width=1920")
+    options.add_argument("--height=1080")
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get("https://qa-scooter.praktikum-services.ru/")
+    yield driver
+    driver.quit()
