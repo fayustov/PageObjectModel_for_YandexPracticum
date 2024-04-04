2bd5ebe09cb2b4558cf71b7b71729f276ed3f816
fayustov <fayustov26@mail.ru>
2024-04-04 11:20:50 +0300

    Отработал финальные замечания по тестам, добавил в проект Аllure отчет

--- tests/test_click_for_logo_scooter.py
+++ tests/test_click_for_logo_scooter.py
@@ -9,22 +9,20 @@ class TestLogoScooter:
     def test_jump_after_click_logo_scooter_in_header(self, driver):
         main_page = MainPage(driver)
         order_button = OrderPage(driver)
-        logo_button = MainPage(driver)
 
         main_page.go_to_site("https://qa-scooter.praktikum-services.ru/")
         order_button.order_for_header()
-        logo_button.jump_after_click_logo_scooter()
+        main_page.jump_after_click_logo_scooter()
         url = main_page.current_url()
         assert url == "https://qa-scooter.praktikum-services.ru/"
 
     @allure.title('Переход на главную, через логотип Самокат (из меню Заказать в футере)')
     def test_jump_after_click_logo_scooter_in_footer(self, driver):
         order_button = OrderPage(driver)
-        logo_button = MainPage(driver)
         main_page = MainPage(driver)
         main_page.go_to_site("https://qa-scooter.praktikum-services.ru/")
         main_page.check_cookie()
         order_button.order_for_footer()
-        logo_button.jump_after_click_logo_scooter()
+        main_page.jump_after_click_logo_scooter()
         url = main_page.current_url()
         assert url == "https://qa-scooter.praktikum-services.ru/"
